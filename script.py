4ac2eb3cacde4234fda8d9e62958757c6d5b8167
jmpjck <siran0@mail.ru>
2018-01-21 00:33:12 +0300

    tst

--- script.py
+++ script.py
@@ -30,7 +30,7 @@ def webhook():
     bot.set_webhook(url="https://testplafrorm.herokuapp.com/bot" + api_token)
     return "!", 200
 
-server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 webhook()
+server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 server = Flask(__name__)
